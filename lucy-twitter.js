66250bcbab43b429c587515689bab99b86994286
Lucy <lucy@bbrennan.info>
2014-12-13 19:20:59 +0000

    Lucy commit

---
+++ lucy-twitter.js
@@ -0,0 +1,32 @@
+var HttpClient = require('https');
+
+exports.getTweetsNearWhitehouse = function(query, callback) {
+  var requestOptions = {
+    host: 'api.twitter.com',
+    port: 443,
+    path: '/1.1/search/tweets.json',
+    headers: {},
+    method: 'get',
+  }
+
+  var req = HttpClient.request(requestOptions, function(res) {
+    res.setEncoding('utf8');
+    var data = '';
+    res.on('error', function(err) {
+      callback(err);
+      callback = null;
+    });
+    res.on('data', function(dataIn) { data += dataIn; });
+    res.on('end', function() {
+      if (callback) {
+        data = JSON.parse(data);
+        callback(null, data);
+      }
+    })
+  });
+  var toSend = {
+    'query': query,
+  }
+  req.write(JSON.stringify(toSend) + '\n');
+  req.end();
+}
