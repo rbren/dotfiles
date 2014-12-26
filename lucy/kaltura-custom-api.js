27b7dff00ac29544b1ac305bc4ef3ccdaf285f4f
Lucy <lucy@bbrennan.info>
2014-12-26 18:39:39 +0000

    Lucy commit

---
+++ lucy/kaltura-custom-api.js
@@ -0,0 +1,11 @@
+var Client = require('./kaltura-raw-api.js');
+
+
+exports.getVideosAboutWhiteHouse = function() {
+  return Client. ({
+    'nameLike': 'White House',
+    'orderBy': '+createdAt',
+  });
+}
+ 
+
