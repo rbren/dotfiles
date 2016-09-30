591273858e6e785a5cd91c3b177763f832c380ed
Bobby Brennan <bobby.brennan@gmail.com>
2016-09-30 11:28:41 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
+sudo apt-get update
+sudo apt-get install -y build-essential git python3 python3-pip
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+mkdir git
+git clone https://github.com/bobby-brennan/homedir && cd homedir
 cp ./.bashrc ~/
 cp ./.vimrc ~/
+
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle
+git clone git://github.com/digitaltoad/vim-pug.git
+git clone https://github.com/Quramy/vim-js-pretty-template
+git clone https://github.com/plasticboy/vim-markdown.git
