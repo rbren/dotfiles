b2d3d02f0dc3260c97b6ea2098b118c6d3a263ad
Bobby Brennan <bobby.brennan@gmail.com>
2016-09-30 12:34:22 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y build-essential git python3 python3-pip
+sudo apt-get install -y build-essential git python3 python3-pip php5-cli
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 mkdir ~/.npm-global
