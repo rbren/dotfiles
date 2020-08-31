2540cb43b439665e093f0a2d93504516d8d9a831
Robert Brennan <contact@rbren.io>
2020-08-31 18:00:58 +0000

    factor out more from .bashrc

---
+++ bashrc.d/010.local.sh
@@ -0,0 +1,3 @@
+if [ -f ~/.local-bashrc ]; then
+  source ~/.local-bashrc
+fi
--- bashrc.d/100.cuddlefish.sh
+++ bashrc.d/100.cuddlefish.sh
@@ -1,3 +1,7 @@
+if [ -f ~/.cuddlefish/config ]; then
+        . ~/.cuddlefish/config
+fi
+
 export CUDDLEFISH_PROJECTS_DIR="$HOME/workspace/projects"
 export NO_CD="True"
 alias cc='cuddlectl'
