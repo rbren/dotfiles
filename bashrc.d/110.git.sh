b380cd57bdf07568bb98ffc009e2edd3e7bedd2e
Robert Brennan <contact@rbren.io>
2020-09-01 20:40:21 +0000

    refactor git status

--- bashrc.d/110.git.sh
+++ bashrc.d/110.git.sh
@@ -73,25 +73,38 @@ function maybe_refresh_git_fetch () {
   fi
 }
 
+clean_color="$(tput setaf $TPUT_GREEN)"
+changes_color="$(tput setaf $TPUT_YELLOW)"
+dirty_color="$(tput setaf $TPUT_RED)"
+unknown_indicator="$changes_color?"
+behind_master_indicator="$dirty_color↓"
+conflict_indicator="$dirty_color↕"
+behind_indicator="$clean_color↓"
+ahead_indicator="$clean_color↑"
+new_branch_indicator="$changes_color↑"
+clean_indicator="$clean_color✓"
+
 function parse_git_status () {
   git_status="$(quiet_git status 2> /dev/null)"
   if [ $? -ne 0 ]; then
     return
   fi
+
   maybe_refresh_git_fetch
   quiet_git rev-parse --git-dir &> /dev/null
   branch="$(parse_git_branch 2> /dev/null)"
   dirty_status="Changes not staged"
   clean_status="working (.*) clean"
   if [[ ${git_status} =~ ${clean_status} ]]; then
-    branch_color="$(tput setaf $TPUT_GREEN)"
+    branch_color=$clean_color
   elif [[ ${git_status} =~ ${dirty_status} ]]; then
-    branch_color="$(tput setaf $TPUT_RED)"
+    branch_color=$dirty_color
   else
-    branch_color="$(tput setaf $TPUT_YELLOW)"
+    branch_color=$changes_color
   fi
+
   if [[ ${branch} =~ " detached " || ${branch} =~ "no branch" || -z "$(quiet_git remote -v)" || -z "$(quiet_git branch --format='%(upstream)' --list master)" ]]; then
-    status_indicator="$(tput setaf $TPUT_YELLOW)?"
+    status_indicator=$unknown_indicator
   else
     branch_exists="0"
     branch_status="$(quiet_git rev-list --left-right --count origin/master...$branch)"
@@ -105,19 +118,19 @@ function parse_git_status () {
     ahead_branch="$(echo $branch_status | sed '$s/.*  *//')"
 
     if [[ ${behind_master} -ne 0 && ${branch} != "master" ]]; then
-      status_indicator="$(tput setaf $TPUT_RED)↓"
+      status_indicator=$behind_master_indicator
     elif [[ ${behind_branch} -ne 0 && ${ahead_branch} -ne 0 ]]; then
-      status_indicator="$(tput setaf $TPUT_RED)↕"
+      status_indicator=$conflict_indicator
     elif [[ ${behind_branch} -ne 0 ]]; then
-      status_indicator="$(tput setaf $TPUT_GREEN)↓"
+      status_indicator=$behind_indicator
     elif [[ ${ahead_branch} -ne 0 ]]; then
       if [[ ${branch_exists} -eq 1 ]]; then
-        status_indicator="$(tput setaf $TPUT_GREEN)↑"
+        status_indicator=$ahead_indicator
       else
-        status_indicator="$(tput setaf $TPUT_YELLOW)↑"
+        status_indicator=$new_branch_indicator
       fi
     else
-      status_indicator="$(tput setaf $TPUT_GREEN)✓"
+      status_indicator=$clean_indicator
     fi
   fi
   echo "$branch_color$branch ${status_indicator}"
