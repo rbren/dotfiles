cb93401f30410f59ae24256622e4fb79d5b3ace5
Robert Brennan <contact@rbren.io>
2021-03-29 16:38:42 +0000

    set aws vault backend

--- bashrc.d/190.cuddlefish.sh
+++ bashrc.d/190.cuddlefish.sh
@@ -2,6 +2,7 @@ if [ -f ~/.cuddlefish/config ]; then
         . ~/.cuddlefish/config
 fi
 
+export AWS_VAULT_BACKEND=file
 export CUDDLEFISH_PROJECTS_DIR="$HOME/workspace/projects"
 export NO_CD="True"
 alias cc='cuddlectl'
