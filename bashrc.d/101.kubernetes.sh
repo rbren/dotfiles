32a2c499ab20e09ce16dcfe044e0d40d4e7d6fc7
Robert Brennan <contact@rbren.io>
2021-07-27 12:58:00 +0000

    fix kkind

--- bashrc.d/101.kubernetes.sh
+++ bashrc.d/101.kubernetes.sh
@@ -7,7 +7,7 @@ alias kw='watch kubectl';
 alias kns='kubectl config set-context --current --namespace '
 alias kport='kubectl port-forward --address 0.0.0.0'
 alias klog='stern --output raw'
-alias kkind='export KUBECONFIG=$HOME/.kube/kind-config-kind'
+alias kkind='kind get kubeconfig > $HOME/.kube/kind-config-kind && export KUBECONFIG=$HOME/.kube/kind-config-kind'
 alias kindpause='docker ps | grep kindest | cut -d" " -f 1 | xargs docker pause'
 alias kindunpause='docker ps | grep kindest | cut -d" " -f 1 | xargs docker unpause'
 
