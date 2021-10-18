20c68b1c45c6c21c56478dbf295b93c01dcf03a7
Robert Brennan <rbren@Roberts-MacBook-Pro.local>
2021-10-18 18:36:25 -0400

    fix docker cmd

--- dotfiles/bashrc.d/100.docker.sh
+++ dotfiles/bashrc.d/100.docker.sh
@@ -2,7 +2,7 @@ alias d='sudo docker';
 alias dockerstop='d stop $(d ps -a -q)';
 alias dockerrm='d rm $(d ps -a -q)';
 alias dockerrmi='d rmi $(d images -f "dangling=true" -q)';
-alias dockerrmif="d rmi $(d images -q)";
+alias dockerrmif='d rmi $(d images -q)';
 alias dockercleanproc='d ps -aq --no-trunc | xargs sudo docker rm'
 alias dockercleanimg='d images -q --filter dangling=true | xargs sudo docker rmi'
 alias dockerpurgeimg='d images -q | xargs sudo docker rmi -f'
