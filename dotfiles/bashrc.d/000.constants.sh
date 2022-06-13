aaa4d91d205331448781ff64ce7b3250d71a8ab3
Robert Brennan <contact@rbren.io>
2022-06-13 17:27:04 +0000

    fix up go env

--- dotfiles/bashrc.d/000.constants.sh
+++ dotfiles/bashrc.d/000.constants.sh
@@ -2,9 +2,8 @@ export IP_ADDRESS=$(curl -s http://whatismyip.akamai.com/ --max-time 5)
 
 export IGNOREEOF=42
 
-export GOROOT=/usr/local/go
+export GOROOT="$(asdf where golang)/go"
 export GOPATH=$HOME/git/go
-export GO_MAIN_DIR="$GOPATH/src/github.com/fairwindsops"
 
 export COLOR_NC='\e[0m' # No Color
 export COLOR_WHITE='\e[1;37m'
--- dotfiles/bashrc.d/150.go.sh
+++ dotfiles/bashrc.d/150.go.sh
@@ -1,1 +1,2 @@
+export GO_MAIN_DIR="$GOPATH/src/github.com/fairwindsops"
 
