c7cdd39e490c818fa83508743b1cd4f488c7d351
Robert Brennan <contact@rbren.io>
2022-07-01 10:58:47 -0400

    simpler tmux history

--- dotfiles/bashrc.d/160.tmux.sh
+++ dotfiles/bashrc.d/160.tmux.sh
@@ -8,11 +8,7 @@ HISTS_DIR=$HOME/.bash_history.d
 mkdir -p "${HISTS_DIR}"
 
 if [ -n "${TMUX_PANE}" ]; then
-  pane_id=$(tmux display-message -p '#{pane_title}')
-  if [[ $pane_id != "$pane_id_prefix"* ]]; then
-    printf "\033]2;$pane_id_prefix`randstr 16`\033\\"
-    pane_id=$(tmux display-message -p '#{pane_title}')
-  fi
+  pane_id=$(echo $TMUX_PANE | sed 's/[^0-9]//g')
   export HISTFILE="${HISTS_DIR}/bash_history_tmux_${pane_id}"
 else
   export HISTFILE="${HISTS_DIR}/bash_history_no_tmux"
