2485473cba32f4044c298dadf8bf8be4447c26f5
Robert Brennan <contact@rbren.io>
2024-08-01 12:15:59 -0400

    add gcloud

--- setup/ops.sh
+++ setup/ops.sh
@@ -39,3 +39,13 @@ asdf_install helm-docs https://github.com/sudermanjr/asdf-helm-docs.git
 
 echo "installing Trivy"
 asdf_install trivy https://github.com/zufardhiyaulhaq/asdf-trivy.git
+
+echo "installing gcloud"
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates gnupg curl
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get install -y google-cloud-cli-app-engine-java
+
+
+
