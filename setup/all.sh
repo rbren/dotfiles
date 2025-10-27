3611fa21e1cc44cd7d00d6eed522045f58f5a958
Robert Brennan <contact@rbren.io>
2025-10-27 16:49:34 +0000

    add logs

--- setup/all.sh
+++ setup/all.sh
@@ -8,12 +8,20 @@ export ARCH_STRING_SECONDARY="x86_64"
 # Disable interactive prompts during install
 sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
 
+echo -e "\n\n\n===================INSTALLERS=================\n\n\n"
 ./setup/installers.sh
+
+echo -e "\n\n\n===================LANGUAGES=================\n\n\n"
 ./setup/languages.sh
+echo -e "\n\n\n===================UTILS=================\n\n\n"
 ./setup/utils.sh
+echo -e "\n\n\n===================GIT=================\n\n\n"
 ./setup/git.sh
+echo -e "\n\n\n===================VIM=================\n\n\n"
 ./setup/vim.sh
+echo -e "\n\n\n===================OPS=================\n\n\n"
 ./setup/ops.sh
+echo -e "\n\n\n===================DOTFILES=================\n\n\n"
 ./setup/dotfiles.sh
 
 echo -e "\n\n\n"
